af60017e2572ae40aebc882cf655def8c003e71f
mprinceps <64213121+bcp2415@users.noreply.github.com>
2021-10-07 16:34:28 +0000

    Adds code to complete challenges b and c.

--- index.js
+++ index.js
@@ -7,12 +7,40 @@ let happiness = ['Hope', 'Joy', 'Peace', 'Love', 'Kindness', 'Puppies', 'Kittens
 
 let words = ['Hello', 'World', 'Python', 'JavaScript', 'Rutabaga'];
 
-for (i=0; i < 3; i++){
-  console.log(randomSelection(happiness));
-  console.log(randomSelection(words));
-}
+
+
 
 //Experiment with the code above. Try to:
 //a) Print 3 random selections from each array.
+
+/*for (i=0; i < 3; i++){
+  console.log(randomSelection(happiness));
+  console.log(randomSelection(words));
+}
+*/
+
 //b) Have the code randomly pick one array, and then print 2 random items from it.
+
+const arrayContainer = [happiness, words];
+
+/*
+function print2FromRandomArray() {
+  const arrayChoice = Math.floor(Math.random() * 2);
+
+  for (let i = 0; i < 2; i++) {
+    console.log(randomSelection(arrayContainer[arrayChoice]));
+  };
+}
+
+
+print2FromRandomArray();
+*/
+
 //c) Create a new array, then fill it with one random item from words and happiness. Print the new array.
+
+let newArray = [];
+
+for (let i = 0; i < 2; i++) {
+  newArray.push(randomSelection(arrayContainer[i]));
+};
+console.log(newArray);
